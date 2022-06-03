b7255b19dc318db27794f79894e30455d429cfb1
cesar lopes <csrwk21@gmail.com>
2022-06-03 16:33:04 -0300

    Paginação tabela solicitantes

--- Front-End/agenda/src/app/views/solicitante-crud/solicitante-crud.component.ts
+++ Front-End/agenda/src/app/views/solicitante-crud/solicitante-crud.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator, MatPaginatorIntl, PageEvent } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 import { Router } from "@angular/router";
 import { SolicitanteService } from 'src/app/components/solicitante/solicitante.service';
 
@@ -14,13 +16,31 @@ export class SolicitanteCrudComponent implements OnInit {
   solicitantes: any;
   filteredSolicitantes: any[] = [];
   _filterby!: string;
+  lowValue = 0
+  highValue = 20;
+  totalRows = 0;
+  pageSize = 5;
+  currentPage = 0;
+  pageSizeOptions: number[] = [5, 10, 25, 100];
 
-  constructor(private router: Router, private service: SolicitanteService) { 
+  displayedColumns: string[] = ['nome', 'cpf', 'email', 'acoes'];
+
+  @ViewChild(MatPaginator)
+  paginator!: MatPaginator;
+
+  dataSource: MatTableDataSource<any> = new MatTableDataSource();
+
+  constructor(private router: Router, private service: SolicitanteService, public _MatPaginatorIntl: MatPaginatorIntl) { 
 
   }
 
   ngOnInit(): void {
+    this._MatPaginatorIntl.itemsPerPageLabel = 'items por Página';
     this.listSolicitantes();
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
   }
 
   listSolicitantes(): void {
@@ -28,7 +48,12 @@ export class SolicitanteCrudComponent implements OnInit {
       .subscribe(
         data => {
           this.solicitantes = data;
-          this.filteredSolicitantes = this.solicitantes;
+          this.dataSource.data = this.solicitantes;
+          setTimeout(() => {
+            this.paginator.pageIndex = this.currentPage;
+            this.paginator.length = data.count; 
+            this.totalRows = this.solicitantes.length;
+          });
         },
         error => {
           console.log(error);
@@ -37,7 +62,7 @@ export class SolicitanteCrudComponent implements OnInit {
 
   set filter(value:string){
     this._filterby = value;
-    this.filteredSolicitantes = this.solicitantes.filter((sol:any)=>sol.nome.toLocaleLowerCase().indexOf(this._filterby.toLocaleLowerCase()) > -1);
+    this.dataSource.data = this.solicitantes.filter((sol:any)=>sol.nome.toLocaleLowerCase().indexOf(this._filterby.toLocaleLowerCase()) > -1);
 
   }
   get filter(){
@@ -47,6 +72,17 @@ export class SolicitanteCrudComponent implements OnInit {
   navigateToSolicitanteCreate(): void{
     this.router.navigate(['/solicitante/create'])
   }
-  
 
+  // public getPaginatorData(event: PageEvent): PageEvent {
+  //   this.lowValue = event.pageIndex * event.pageSize;
+  //   this.highValue = this.lowValue + event.pageSize;
+  //   return event;
+  // }
+
+  pageChanged(event: PageEvent) {
+    console.log({ event });
+    this.pageSize = event.pageSize;
+    this.currentPage = event.pageIndex;
+    //this.listSolicitantes();
+  }
 }
