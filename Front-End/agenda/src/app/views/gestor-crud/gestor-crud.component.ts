83f746db878b0420521e2f1fa119303f61e9d35c
cesar lopes <csrwk21@gmail.com>
2022-06-03 22:54:09 -0300

    Paginação tabela gestor

--- Front-End/agenda/src/app/views/gestor-crud/gestor-crud.component.ts
+++ Front-End/agenda/src/app/views/gestor-crud/gestor-crud.component.ts
@@ -1,6 +1,8 @@
 import { GestorService } from './../../components/gestor/gestor.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router }from "@angular/router";
+import { MatPaginator, MatPaginatorIntl, PageEvent } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 @Component({
   selector: 'app-gestor-crud',
   templateUrl: './gestor-crud.component.html',
@@ -8,17 +10,41 @@ import { Router }from "@angular/router";
 })
 export class GestorCrudComponent implements OnInit {
 
-  constructor(private router: Router, private service:GestorService) { }
+  totalRows = 0;
+  pageSize = 5;
+  currentPage = 0;
+  pageSizeOptions: number[] = [5, 10, 25, 100];
+
+  displayedColumns: string[] = ['nome', 'regiao', 'telefone', 'acoes'];
+
+  @ViewChild(MatPaginator)
+  paginator!: MatPaginator;
+
+  dataSource: MatTableDataSource<any> = new MatTableDataSource();
+
+  constructor(private router: Router, private service:GestorService, public _MatPaginatorIntl: MatPaginatorIntl) { }
   gestores: any;
+  
   ngOnInit(): void {
+    this._MatPaginatorIntl.itemsPerPageLabel = 'items por Página';
     this.listGestores();
   }
   
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
   listGestores(): void {
     this.service.list()
       .subscribe(
         data => {
           this.gestores = data;
+          this.dataSource.data = this.gestores;
+          setTimeout(() => {
+            this.paginator.pageIndex = this.currentPage;
+            this.paginator.length = data.count; 
+            this.totalRows = this.gestores.length;
+          })
           console.log(data);
         },
         error => {
@@ -28,6 +54,12 @@ export class GestorCrudComponent implements OnInit {
 
   navigateToGestorCreate(): void{
     this.router.navigate(['/gestor/create'])
+  }
+
+  pageChanged(event: PageEvent) {
+    console.log({ event });
+    this.pageSize = event.pageSize;
+    this.currentPage = event.pageIndex;
   }
 
 }
