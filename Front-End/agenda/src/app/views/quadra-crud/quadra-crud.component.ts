3cc257e30ae425b3cfd3344e2e01099663257182
cesar lopes <csrwk21@gmail.com>
2022-06-03 22:34:20 -0300

    Paginação tabela quadras

--- Front-End/agenda/src/app/views/quadra-crud/quadra-crud.component.ts
+++ Front-End/agenda/src/app/views/quadra-crud/quadra-crud.component.ts
@@ -1,6 +1,8 @@
 import { QuadraService } from './../../components/quadra/quadra.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from "@angular/router";
+import { MatPaginator, MatPaginatorIntl, PageEvent } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 @Component({
   selector: 'app-quadra-crud',
   templateUrl: './quadra-crud.component.html',
@@ -8,12 +10,27 @@ import { Router } from "@angular/router";
 })
 export class QuadraCrudComponent implements OnInit {
 
-  constructor( private router: Router, private service:QuadraService) { }
-
   quadras: any;
+  totalRows = 0;
+  pageSize = 5;
+  currentPage = 0;
+  pageSizeOptions: number[] = [5, 10, 25, 100];
+
+  displayedColumns: string[] = ['nome', 'qtd_pessoas', 'regiao', 'acoes'];
+
+  @ViewChild(MatPaginator)
+  paginator!: MatPaginator;
+
+  dataSource: MatTableDataSource<any> = new MatTableDataSource();
+
+  constructor( private router: Router, private service:QuadraService, public _MatPaginatorIntl: MatPaginatorIntl) { }
 
   ngOnInit(): void {
     this.listQuadras();
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
   }
 
 
@@ -22,6 +39,12 @@ export class QuadraCrudComponent implements OnInit {
       .subscribe(
         data => {
           this.quadras = data;
+          this.dataSource.data = this.quadras;
+          setTimeout(() =>{
+            this.paginator.pageIndex = this.currentPage;
+            this.paginator.length = data.count; 
+            this.totalRows = this.quadras.length;
+          });
           console.log(data);
         },
         error => {
@@ -30,5 +53,11 @@ export class QuadraCrudComponent implements OnInit {
   }
   navigateToQuadraCreate(): void{
     this.router.navigate(['/quadra/create'])
+  }
+
+  pageChanged(event: PageEvent) {
+    console.log({ event });
+    this.pageSize = event.pageSize;
+    this.currentPage = event.pageIndex;
   }
 }
